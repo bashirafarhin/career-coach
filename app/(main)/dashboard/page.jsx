b8374a57c787b29870da6bf6940def96d524ebd4
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-04-04 20:29:56 +0530

    corrected onboarding status

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,15 @@ import { redirect } from "next/navigation";
 import DashboardView from "./_components/dashboard-view";
 
 const IndustryInsightsPage = async () => {
-  const { isOnboarded } = await getUserOnboardingStatus();
-  const insights = await getIndustryInsights();
-
+  // const { isOnboarded } = await getUserOnboardingStatus();
+  const isOnboarded = await getUserOnboardingStatus();
   if (!isOnboarded) {
     redirect("/onboarding");
   }
+
+  console.log("isOnboarded", isOnboarded)
+  const insights = await getIndustryInsights();
+
   return (
     <div className="container mx-auto">
       <DashboardView insights={insights} />
